466e64b5c896532731c9a679aca2f67dd05c599b
Deeptanshukar <swatihati123@gmail.com>
2026-01-19 21:05:10 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -42,4 +42,37 @@ socket.on("history", messages => {
 
   chatBox.scrollTop = chatBox.scrollHeight;
 });
+// Function to add a message to the UI
+function appendMessage(user, text, time) {
+    const messagesDiv = document.getElementById("messages");
+    const msgElement = document.createElement("div");
+    msgElement.classList.add("message-container");
+
+    // Format the time (If no time is provided, use the current time)
+    const msgDate = time ? new Date(time) : new Date();
+    const timeString = msgDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+    msgElement.innerHTML = `
+        <span class="message-user"><b>${user}</b></span>
+        <span class="message-text">${text}</span>
+        <span class="message-time" style="font-size: 0.7em; color: gray; margin-left: 10px;">
+            ${timeString}
+        </span>
+    `;
+
+    messagesDiv.appendChild(msgElement);
+    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+}
+
+// Update the history listener to pass the saved time
+socket.on("chat-history", (messages) => {
+    messages.forEach(msg => {
+        appendMessage(msg.user, msg.text, msg.time); 
+    });
+});
+
+// Update the live message listener
+socket.on("message", (data) => {
+    appendMessage(data.user, data.text, new Date());
+});
 
